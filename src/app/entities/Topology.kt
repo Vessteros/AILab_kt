e0573e042f3f4ad324f0413fd2b32d998a8c54b9
Nikita Mogilenko <nikita.mogilenko@doconcall.ru>
2019-04-30 14:42:21 +0300

    мех

--- src/app/entities/Topology.kt
+++ src/app/entities/Topology.kt
@@ -4,7 +4,7 @@ object Topology {
     /**
      * Сама топология
      */
-    var topology: Array<out IntArray> = Array(0, {IntArray(0, {0})})
+    var topology: Array<out IntArray> = Array(0) {IntArray(0) {0} }
 
     /**
      * Количество вершин топологии
